9e253347a536564a431ae31c1d362e2d9d0365cd
mdj <2menno@gmail.com>
2012-11-20 15:42:06 +0100

    Add 5 screens

--- BTDT_6/gen/com/androidbook/btdt/hour6/R.java
+++ BTDT_6/gen/com/androidbook/btdt/hour6/R.java
@@ -17,15 +17,25 @@ public final class R {
         public static final int menu_settings=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_quiz_splash=0x7f030000;
+        public static final int activity_quiz_game=0x7f030000;
+        public static final int activity_quiz_help=0x7f030001;
+        public static final int activity_quiz_menu=0x7f030002;
+        public static final int activity_quiz_scores=0x7f030003;
+        public static final int activity_quiz_settings=0x7f030004;
+        public static final int activity_quiz_splash=0x7f030005;
     }
     public static final class menu {
         public static final int activity_quiz_splash=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int game=0x7f040005;
+        public static final int help=0x7f040001;
+        public static final int menu=0x7f040002;
+        public static final int menu_settings=0x7f040007;
+        public static final int scores=0x7f040006;
+        public static final int settings=0x7f040004;
+        public static final int splash=0x7f040003;
     }
     public static final class style {
         /** 
