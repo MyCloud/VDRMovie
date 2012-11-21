bc20f5811c6ae9c6932db5c94687d858629b3a5d
mdj <2menno@gmail.com>
2012-11-21 11:58:24 +0100

    Add lastDate in preference

--- BTDT_6/src/com/androidbook/btdt/hour6/QuizActivity.java
+++ BTDT_6/src/com/androidbook/btdt/hour6/QuizActivity.java
@@ -1,15 +1,26 @@
 package com.androidbook.btdt.hour6;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.Menu;
 
 public class QuizActivity extends Activity {
-
+	public static final String PREFS_NAME = "MyPrefsFile";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		Date now = new Date();
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz_splash);
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+	    // read last date if null than use current date
+		String last = settings.getString("lastDate", now.toString());
+	    Log.i(PREFS_NAME, last );
 	}
 
 	@Override
@@ -17,6 +28,23 @@ public class QuizActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_quiz_splash, menu);
 		return true;
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		Date now = new Date();
+		// save all settings
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		SharedPreferences.Editor prefEditor = settings.edit();
+		prefEditor.putString("LastDate", now.toString());
+		super.onPause();
 	}
 
 }
--- BTDT_6/src/com/androidbook/btdt/hour6/QuizSplashActivity.java
+++ BTDT_6/src/com/androidbook/btdt/hour6/QuizSplashActivity.java
@@ -1,6 +1,7 @@
 package com.androidbook.btdt.hour6;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class QuizSplashActivity extends QuizActivity {
 
@@ -8,6 +9,7 @@ public class QuizSplashActivity extends QuizActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz_splash);
+		Log.i("test", "create");
 	}
 
 }
