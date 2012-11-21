ab42031c0f51bde042ba0cae68912c1ef50f4789
mdj <2menno@gmail.com>
2012-11-21 19:44:41 +0100

    hour 6 done

--- BTDT_6/src/com/androidbook/btdt/hour6/QuizSplashActivity.java
+++ BTDT_6/src/com/androidbook/btdt/hour6/QuizSplashActivity.java
@@ -1,7 +1,15 @@
 package com.androidbook.btdt.hour6;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class QuizSplashActivity extends QuizActivity {
 
@@ -9,7 +17,58 @@ public class QuizSplashActivity extends QuizActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz_splash);
-		Log.i("test", "create");
+        // Fade in top title
+        TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
+        Animation fade1 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo1.startAnimation(fade1);
+        // Fade in bottom title after a built-in delay.
+        TextView logo2 = (TextView) findViewById(R.id.TextViewBottomTitle);
+        Animation fade2 = AnimationUtils.loadAnimation(this, R.anim.fade_in2);
+        logo2.startAnimation(fade2);
+         // Transition to Main Menu when bottom title finishes animating
+        fade2.setAnimationListener(new AnimationListener() {
+            public void onAnimationEnd(Animation animation) {
+                // The animation has ended, transition to the Main Menu screen
+                startActivity(new Intent(QuizSplashActivity.this, QuizMenuActivity.class));
+                QuizSplashActivity.this.finish();
+            }
+
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            public void onAnimationStart(Animation animation) {
+            }
+        });
+        // Load animations for all views within the TableLayout
+        Animation spinin = AnimationUtils.loadAnimation(this, R.anim.custom_anim);
+        LayoutAnimationController controller = new LayoutAnimationController(spinin);
+        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.setLayoutAnimation(controller);
+        }
+
+
+
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+        // Stop the animation
+        TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
+        logo1.clearAnimation();
+        TextView logo2 = (TextView) findViewById(R.id.TextViewBottomTitle);
+        logo2.clearAnimation();
+        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.clearAnimation();
+        }
+
+
+
 	}
 
 }
